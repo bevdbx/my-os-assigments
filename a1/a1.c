5886980ff1020f0931e69c4e2b006cef6ec1bf27
bevdbx <bevdbx607@gmail.com>
2023-04-07 21:05:48 +0300

    vineri seara

--- a1/a1.c
+++ a1/a1.c
@@ -5,6 +5,7 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 void listRecSingle(const char *path)
 {
@@ -98,7 +99,7 @@ void permCheck(const char *path, const char *permission)
             
             char *a = malloc(10);
 
-	    snprintf(a, 10, "%c%c%c%c%c%c%c%c%c",
+	    snprintf(a, 10, "%c%c%c%c%c%c%c%c%c",          //luat de pe net snprintf-uls
              (statbuf.st_mode & S_IRUSR) ? 'r' : '-',
              (statbuf.st_mode & S_IWUSR) ? 'w' : '-',
              (statbuf.st_mode & S_IXUSR) ? 'x' : '-',
@@ -143,7 +144,7 @@ void permCheckRecursive(const char *path, const char *permission)
             
             char *a = malloc(10);
 
-        snprintf(a, 10, "%c%c%c%c%c%c%c%c%c",
+        snprintf(a, 10, "%c%c%c%c%c%c%c%c%c",                //luat de pe net snprintf-ul
              (statbuf.st_mode & S_IRUSR) ? 'r' : '-',
              (statbuf.st_mode & S_IWUSR) ? 'w' : '-',
              (statbuf.st_mode & S_IXUSR) ? 'x' : '-',
@@ -246,6 +247,170 @@ void fileSizeRecursive(const char* dirPath, const int f_size)
     closedir(dir);
 }
 
+void parsing(const char* path)
+{
+    int f = -1;
+    f = open(path, O_RDONLY);
+    if(f == -1)
+        return ;
+
+    lseek(f,0,SEEK_SET);
+    char a[5], d[256][128];
+    int b=0, c=0, e[256] = {0}, g[256] = {0};
+    read(f,a,4);
+    a[4] = '\0';
+    lseek(f,6,SEEK_SET);
+    read(f,&b,2);
+    read(f,&c,1);
+    for(int i = 0; i < c; i++)
+    {
+        lseek(f,9+i*19,SEEK_SET);
+        read(f,d[i],7);
+        d[i][7] = '\0';
+        lseek(f,16+i*19,SEEK_SET);
+        read(f,&e[i],4);
+        lseek(f,24+i*19,SEEK_SET);
+        read(f,&g[i],4);
+    }
+
+    if(strcmp(a,"BxQS") != 0)
+    {
+        printf("ERROR\nwrong magic\n");
+        close(f);
+        return ;
+    }
+    if(b < 124 || b > 221)
+    {
+        printf("ERROR\nwrong version\n");
+        close(f);
+        return ;
+    }
+    if(c < 6 || c > 13)
+    {
+        printf("ERROR\nwrong sect_nr\n");
+        close(f);
+        return ;
+    }
+    int z = 0;
+    for(int i = 0; i < c; i++)
+    {
+        if(e[i] != 96 && e[i] != 72 && e[i] != 79 && e[i] != 35 && e[i] != 63)
+            z = 1;
+    }
+    if(z == 1)
+    {
+        printf("ERROR\nwrong sect_types\n");
+        close(f);
+        return ;
+    }
+    printf("SUCCESS\nversion=%d\n",b);
+    printf("nr_sections=%d\n",c);
+    for(int i = 1; i <=c; i++)
+    {
+        printf("section%d: %s %d %d\n",i, d[i-1], e[i-1], g[i-1]);
+        close(f);
+    }
+    close(f);
+}
+
+int pentru_findall(const char* path)
+{
+    int f = -1;
+    f = open(path, O_RDONLY);
+    if(f == -1)
+        return -1;
+
+    lseek(f,0,SEEK_SET);
+    char a[5], d[256][128];
+    int b=0, c=0, e[256] = {0}, g[256] = {0};
+    read(f,a,4);
+    a[4] = '\0';
+    lseek(f,6,SEEK_SET);
+    read(f,&b,2);
+    read(f,&c,1);
+    for(int i = 0; i < c; i++)
+    {
+        lseek(f,9+i*19,SEEK_SET);
+        read(f,d[i],7);
+        d[i][7] = '\0';
+        lseek(f,16+i*19,SEEK_SET);
+        read(f,&e[i],4);
+        lseek(f,24+i*19,SEEK_SET);
+        read(f,&g[i],4);
+    }
+
+    if(strcmp(a,"BxQS") != 0)
+    {
+        close(f);
+        return -1;
+    }
+    if(b < 124 || b > 221)
+    {
+        close(f);
+        return -1;
+    }
+    if(c < 6 || c > 13)
+    {
+        close(f);
+        return -1;
+    }
+    int z = 0, w = 0;
+    for(int i = 0; i < c; i++)
+    {
+        if(e[i] != 96 && e[i] != 72 && e[i] != 79 && e[i] != 35 && e[i] != 63)
+            z = 1;
+        if(e[i] == 35)
+            w++;
+    }
+    if(z == 1)
+    {
+        close(f);
+        return -1;
+    }
+    close(f);
+    return w;
+}
+
+void findall(const char* dirPath)
+{
+    DIR *dir = NULL;
+    struct dirent *entry = NULL;
+    char fullPath[512];
+    struct stat statbuf;
+
+    dir = opendir(dirPath);
+    if(dir == NULL)
+    {
+        perror("opendir");
+        return ;
+    }
+    while((entry = readdir(dir)) != NULL)
+    {
+        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+        {
+            snprintf(fullPath, 512, "%s/%s", dirPath, entry->d_name);
+            if(lstat(fullPath, &statbuf) != 0)
+            {
+                perror("lstat");
+                continue;
+            }
+
+            if(S_ISREG(statbuf.st_mode))
+            {
+                if(pentru_findall(fullPath) > 0)
+                    printf("%s\n", fullPath);
+            }
+            else
+            {
+                if(S_ISDIR(statbuf.st_mode))
+                    findall(fullPath);
+
+            }
+        }
+    }
+    closedir(dir);
+}
+
 
 int main(int argc, char **argv){
     if(argc >= 2){
@@ -333,7 +498,30 @@ int main(int argc, char **argv){
                             }
                         }
                     }
-                    
+                }
+                else
+                {
+                    if(strcmp(argv[1], "parse") == 0)
+                    {
+                        if(argv[2][0] == 'p' && argv[2][1] == 'a' && argv[2][2] == 't')
+                        {
+                            char *str = malloc(strlen(argv[2])+1);
+                            strcpy(str, argv[2]+5);
+                            parsing(str);
+                            free(str);
+                        }
+                    }
+                    else
+                    {
+                        if(strcmp(argv[1], "findall") == 0)
+                        {
+                            char *str = malloc(strlen(argv[2])+1);
+                            strcpy(str, argv[2]+5);
+                            printf("SUCCESS\n");
+                            findall(str);
+                            free(str);
+                        }
+                    }
                 }
             }
         }
