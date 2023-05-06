b4b117f3efcdef8b1fb1db938e3f2317af7ae5a9
bevdbx <bevdbx607@gmail.com>
2023-05-06 23:44:09 +0300

    commit2

--- a2/a2.c
+++ a2/a2.c
@@ -4,8 +4,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include "a2_helper.h"
-
+/*
 void *thread_f(void* arg)
 {
     info(BEGIN, 2, 1);
@@ -46,6 +49,7 @@ void *thread_func(void* arg)
             info(END, 3, i);
         }
     }
+
     info(BEGIN, 3, 1);
 
     info(BEGIN, 3, 2);
@@ -93,82 +97,120 @@ void *thread_function(void* arg)
 
     return NULL;
 }
+*/
+
+sem_t semafor1, semafor2;
+sem_t *sem1, *sem2;
+
+void* thread_2(void* arg)
+{
+    int id2 = *(int *)arg;
+
+    if(id2 == 2)
+    {
+        info(BEGIN, 2, id2);
+    
+        info(END, 2, id2);
+        sem_post(sem1);
+    }
+    else if(id2 == 1)
+    {
+        sem_wait(sem2);
+        info(BEGIN, 2, id2);
+        
+        info(END, 2, id2);
+    }
+    else
+    {
+        info(BEGIN, 2, id2);
+        
+        info(END, 2, id2);
+    }
+    return NULL;
+}
+
+void* thread_3(void* arg)
+{
+    int id3 = *(int *)arg;
+
+    info(BEGIN, 3, id3);
+    
+    info(END, 3, id3);
+
+    return NULL;
+}
+
+void* thread_4(void* arg)
+{
+    int id4 = *(int *)arg;
+    
+    if(id4 == 3)
+    {
+        sem_wait(sem1);
+        info(BEGIN, 4, id4);
+        
+        info(END, 4, id4);
+        sem_post(sem2);
+    }
+    else if(id4 == 4)
+    {
+        info(BEGIN, 4, id4);
+        sem_post(&semafor1);
+        
+        sem_wait(&semafor2);
+        info(END, 4, id4);
+    }
+    else if(id4 == 2)
+    {
+        sem_wait(&semafor1);
+        info(BEGIN, 4, id4);
+        
+        info(END, 4, id4);
+        sem_post(&semafor2);
+    }
+    else
+    {
+        info(BEGIN, 4, id4);
+
+        info(END, 4, id4);
+    }
+    return NULL;
+}
 
 int main(){
-
     init();
-
+    
     info(BEGIN, 1, 0);
     
-    pid_t p2 = fork();
-    //pid_t p4 = 0;
-
-    if(p2 == 0)
-    {
-        info(BEGIN, 2, 0);
-        
-        
-
-        pid_t p4 = fork();
-
-        if(p4 == 0)
-        {
-            info(BEGIN, 4, 0);
-            
-            pthread_t t4 = -1;
-            if(pthread_create(&t4, NULL, thread_function, NULL) != 0)
-            {
-                return -1;
-            }
-            pthread_join(t4,NULL);
-            info(END, 4, 0);
-            exit(4);
-        }
-        else waitpid(p4,NULL,0);
-
-        pid_t p5 = fork();
-
-        if(p5 == 0)
-        {
-            info(BEGIN, 5, 0);
-            
-            info(END, 5, 0);
-            exit(4);
-        }
-        else waitpid(p5,NULL,0);
-
-        pthread_t t2 = -1;
-        if(pthread_create(&t2, NULL, thread_f, NULL) != 0)
-        {
-            return -1;
-        }
-        pthread_join(t2,NULL);
-
-        info(END, 2, 0);
-        exit(4);
-    }
-    else waitpid(p2,NULL,0);
-
     pid_t p3 = fork();
-
+    
     if(p3 == 0)
     {
         info(BEGIN, 3, 0);
         
-        pthread_t t3 = -1;
-        if(pthread_create(&t3, NULL, thread_func, NULL) != 0)
+        pthread_t t3[37];
+        int res3[37];
+        for(int i = 0; i < 37; i++)
         {
-            return -1;
+            res3[i] = i + 1;
+            if(pthread_create(&t3[i], NULL, thread_3, &res3[i]) != 0)
+            {
+                return -1;
+            }
         }
-        pthread_join(t3,NULL);
-
+        
+        for(int i = 0; i < 37; i++)
+        {
+            pthread_join(t3[i], NULL);
+        }
+        
         info(END, 3, 0);
         exit(4);
     }
-    else waitpid(p3,NULL,0);
-
+    else waitpid(p3, NULL, 0);
+    
     pid_t p6 = fork();
-
+    
     if(p6 == 0)
     {
         info(BEGIN, 6, 0);
@@ -176,20 +218,105 @@ int main(){
         info(END, 6, 0);
         exit(4);
     }
-    else waitpid(p6,NULL,0);
-
+    else waitpid(p6, NULL, 0);
+    
     pid_t p7 = fork();
-        
+    
     if(p7 == 0)
     {
         info(BEGIN, 7, 0);
-              
+        
         info(END, 7, 0);
         exit(4);
-    } 
-    else waitpid(p7,NULL,0);
+    }
+    else waitpid(p7, NULL, 0);
+    
+        sem1 = sem_open("sem1", O_CREAT, 0644, 0); 
 
+        sem2 = sem_open("sem2", O_CREAT, 0644, 0); 
+    
+    pid_t p2 = -1, p4 = -1, p5 = -1;
+    p2 = fork();
+    if(p2 == 0)
+    {
+        info(BEGIN, 2, 0);
+        
+        p5 = fork();
+        
+        if(p5 == 0)
+        {
+            info(BEGIN, 5, 0);
+        
+            info(END, 5, 0);
+            exit(4);
+        }
+        else 
+        {
+            waitpid(p5, NULL, 0);
+        
+            p4 = fork();
+            
+            if(p4 == 0)
+            {
+                info(BEGIN, 4, 0);
+            
+                pthread_t t2[6], t4[5];
+                int res2[6], res4[5];
+        
+                sem1 = sem_open("sem1", 0); 
+                    sem2 = sem_open("sem2", 0); 
+        
+                for(int i = 0; i < 6; i++)
+                {
+                    res2[i] = i + 1;
+                    if(pthread_create(&t2[i], NULL, thread_2, &res2[i]) != 0)
+                    {
+                        return -1;
+                    }
+                }
+        
+                for(int i = 0; i < 5; i++)
+                {
+                    res4[i] = i + 1;
+                    if(pthread_create(&t4[i], NULL, thread_4, &res4[i]) != 0)
+                    {
+                        return -1;
+                    }
+                }
+        
+                for(int i = 0; i < 5; i++)
+                {
+                    pthread_join(t4[i], NULL);
+                }
+            
+            
+                for(int i = 0; i < 6; i++)
+                {
+                    pthread_join(t2[i], NULL);
+                }
+                    
+                    info(END, 4, 0);
+                    exit(4);
+                }
+                else
+                {
+                waitpid(p4, NULL, 0);
+        
+                info(END, 2, 0);
+                exit(4);
+            }
+        }
+    }
+    else waitpid(p2, NULL, 0);
+    
+    sem_close(sem1);
+        sem_close(sem2);
+    
     info(END, 1, 0);
     exit(4);
+
     return 0;
 }
+        
+
+    
